e82933b9dda7de944649cb0b28b817abc6e20e1c
=nicholasrier <=nicholasrier.dev@gmail.com>
2018-09-27 15:06:23 -0700

    Email fix

--- libraryCR/src/main/java/com/plusqa/bc/crashreport/FormatAndSend.java
+++ libraryCR/src/main/java/com/plusqa/bc/crashreport/FormatAndSend.java
@@ -1,12 +1,18 @@
 package com.plusqa.bc.crashreport;
 
+import android.Manifest;
 import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.net.Uri;
+import android.os.Environment;
 import android.provider.MediaStore;
+import android.support.annotation.NonNull;
 import android.support.constraint.ConstraintLayout;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.FileProvider;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -20,6 +26,7 @@ import android.view.MotionEvent;
 import android.view.View;
 import android.view.View.OnTouchListener;
 import android.view.inputmethod.InputMethodManager;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import com.android.volley.NetworkResponse;
@@ -35,11 +42,15 @@ import com.android.volley.toolbox.Volley;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 public class FormatAndSend extends AppCompatActivity
         implements ImageRecyclerViewAdapter.ItemClickListener {
@@ -47,6 +58,9 @@ public class FormatAndSend extends AppCompatActivity
     InputMethodManager imm;
     private List<Bitmap> images;
     private ImageRecyclerViewAdapter adapter;
+    private EditText titleText, descriptionText;
+    private String logcatString;
+
 
     @SuppressLint("ClickableViewAccessibility")
     @Override
@@ -57,6 +71,8 @@ public class FormatAndSend extends AppCompatActivity
         Toolbar mTopToolbar = findViewById(R.id.format_and_send_toolbar);
         setSupportActionBar(mTopToolbar);
 
+        titleText = findViewById(R.id.edit_text1);
+        descriptionText = findViewById(R.id.edit_text2);
 
         imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
 
@@ -99,6 +115,10 @@ public class FormatAndSend extends AppCompatActivity
                 return false;
             }
         });
+
+        //grab log from internal storage
+        logcatString = Utils.readLogFromInternalMemory(this.getApplicationContext()).toString();
+
     }
 
     @Override
@@ -119,9 +139,14 @@ public class FormatAndSend extends AppCompatActivity
 
         } else if (item.getItemId() == R.id.action_send) {
 
-            sendReport();
+            // email
+            ActivityCompat.requestPermissions(FormatAndSend.this,
+                    new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                    1);
 
-            this.finishAffinity();
+//            sendReport();
+
+//            this.finishAffinity();
 
             return true;
         }
@@ -178,13 +203,15 @@ public class FormatAndSend extends AppCompatActivity
                     }
                 }
                 break;
+
+            case 2:
+                this.finishAffinity();
         }
     }
 
     private boolean sendReport() {
 
-        //grab log from internal storage
-        String logcatString = Utils.readLogFromInternalMemory(this.getApplicationContext()).toString();
+
 
         // Post to server
         try {
@@ -259,5 +286,111 @@ public class FormatAndSend extends AppCompatActivity
         return true;
     }
 
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           @NonNull String permissions[], @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case 1: {
+
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                    openEmail();
+
+                } else {
+
+                    Toast.makeText(FormatAndSend.this, "Permission denied, unable to open email", Toast.LENGTH_SHORT).show();
+                }
+            }
+            return;
+
+            // other 'case' lines to check for other
+            // permissions this app might request
+        }
+    }
+
+    public void openEmail() {
+
+        String subject = titleText.getText().toString();
+        String body = descriptionText.getText().toString();
+
+        Intent intent = new Intent(Intent.ACTION_SEND_MULTIPLE);
+
+        intent.setType("text/plain");
+
+        intent.setFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+
+        intent.putExtra(Intent.EXTRA_EMAIL  , new String[]{"nicholas@plusqa.com"});
+        intent.putExtra(Intent.EXTRA_SUBJECT, subject);
+        intent.putExtra(Intent.EXTRA_TEXT   , body);
+
+        ArrayList<Uri> uris = new ArrayList<>();
+
+        for (int i = 0; i < images.size(); i++) {
+
+            String imageName = "BetaCrashReporterImage" + i + ".jpg";
+
+            File file = new File (Environment.getExternalStoragePublicDirectory("DIRECTORY_PICTURES"), imageName);
+
+            if (file.exists()) {
+                file.delete();
+            }
+
+            file.getParentFile().mkdirs();
+
+            try {
+                file.createNewFile();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            try {
+                FileOutputStream out = new FileOutputStream(file);
+                images.get(i).compress(Bitmap.CompressFormat.JPEG, 90, out);
+                out.flush();
+                out.close();
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+            Uri pngUri = FileProvider.getUriForFile(FormatAndSend.this,
+                    "com.plusqa.bc.crashreport.provider",
+                    file);
+
+
+            uris.add(pngUri);
+
+        }
+
+        String logFileName = "externalLog.txt";
+
+        File logDir = Environment.getExternalStorageDirectory();
+        File logFile = new File(logDir.getPath(), logFileName);
+        try {
+            FileWriter writer = new FileWriter(logFile);
+            writer.write(logcatString);
+            writer.flush();
+            writer.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        Uri logUri = FileProvider.getUriForFile(FormatAndSend.this,
+                "com.plusqa.bc.crashreport.provider",
+                logFile);
+
+        uris.add(logUri);
+
+        intent.putParcelableArrayListExtra(Intent.EXTRA_STREAM, uris);
+
+        try {
+            startActivityForResult(Intent.createChooser(intent, "Send mail..."), 2);
+        } catch (android.content.ActivityNotFoundException ex) {
+            Toast.makeText(FormatAndSend.this,
+                    "There are no email clients installed.", Toast.LENGTH_SHORT).show();
+        }
+    }
 
 }
